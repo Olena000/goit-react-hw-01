2983c22f8577e4ee6f5534991b6fcadfb21bb863
Olena000 <154604010+Olena000@users.noreply.github.com>
2024-06-30 13:01:02 +0300

    1.1

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,6 @@ export default function App() {
         image={userData.avatar}
         stats={userData.stats}
       />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
     </>
   );
 }
